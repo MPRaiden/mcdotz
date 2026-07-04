59d37a190601c558a2343d7bd7ed0ba7d1d28cb9
Marin Peric Radan <marin.peric@seekandhit.com>
2026-07-04 14:53:27 +0200

    custom theme

--- nvim/.config/nvim/plugin/00-theme.lua
+++ nvim/.config/nvim/plugin/00-theme.lua
@@ -1,66 +1,149 @@
-vim.pack.add { { src = 'https://github.com/catppuccin/nvim', name = 'catppuccin' } }
-vim.pack.add { { src = 'https://github.com/edeneast/nightfox.nvim' } }
+vim.o.termguicolors = true
 
-require('catppuccin').setup {
-  flavour = 'auto', -- latte, frappe, macchiato, mocha
-  transparent_background = true, -- disables setting the background color.
-  float = {
-    transparent = true, -- enable transparent floating windows
-    solid = false, -- use solid styling for floating windows, see |winborder|
-  },
-  no_italic = true, -- Force no italic
-  no_bold = false, -- Force no bold
-  auto_integrations = true,
+local c = {
+  paper = '#f8f1e8',
+  paper_alt = '#f3e8dc',
+  paper_float = '#faf3eb',
+  ink = '#4e4945',
+  ink_soft = '#6b655e',
+  ink_faint = '#9a9086',
+  blue = '#467fbe',
+  blue_soft = '#6c98c0',
+  rose = '#cb8d97',
+  gold = '#c6a45d',
+  orange = '#e39a2d',
+  green = '#7ea17a',
+  red = '#c07a70',
+  select = '#e4d5c4',
+  search = '#ead9a7',
 }
 
--- vim.cmd 'colorscheme catppuccin'
+vim.cmd('highlight clear')
+if vim.fn.exists('syntax_on') == 1 then
+  vim.cmd('syntax reset')
+end
+vim.g.colors_name = 'paper_kakashi'
 
--- Default options
-require('nightfox').setup {
-  options = {
-    transparent = true, -- Disable setting background
-  },
-  groups = {
-    all = {
-      NormalFloat = { bg = 'NONE' },
-      FloatBorder = { fg = 'palette.blue', bg = 'NONE' },
-      FloatTitle = { fg = 'palette.blue', bg = 'NONE' },
+local set = vim.api.nvim_set_hl
+local function hl(group, opts)
+  set(0, group, opts)
+end
 
-      Pmenu = { link = 'NormalFloat' },
-      PmenuBorder = { link = 'FloatBorder' },
-      PmenuSel = { bg = 'sel0' },
-      PmenuSbar = { bg = 'NONE' },
-      PmenuThumb = { bg = 'sel0' },
+hl('Normal', { fg = c.ink, bg = c.paper })
+hl('NormalNC', { fg = c.ink, bg = c.paper })
+hl('NormalFloat', { fg = c.ink, bg = c.paper_float })
+hl('FloatBorder', { fg = c.blue, bg = c.paper_float })
+hl('FloatTitle', { fg = c.blue, bg = c.paper_float, bold = true })
+hl('ColorColumn', { bg = c.paper_alt })
+hl('CursorLine', { bg = c.paper_alt })
+hl('CursorColumn', { bg = c.paper_alt })
+hl('CursorLineNr', { fg = c.blue, bg = c.paper_alt, bold = true })
+hl('LineNr', { fg = c.ink_faint, bg = c.paper })
+hl('SignColumn', { bg = c.paper })
+hl('FoldColumn', { fg = c.ink_faint, bg = c.paper })
+hl('Folded', { fg = c.ink_soft, bg = c.paper_alt })
+hl('VertSplit', { fg = c.paper_alt, bg = c.paper })
+hl('WinSeparator', { fg = c.paper_alt, bg = c.paper })
+hl('Visual', { bg = c.select })
+hl('Search', { fg = c.ink, bg = c.search })
+hl('IncSearch', { fg = c.paper, bg = c.blue })
+hl('MatchParen', { fg = c.rose, bg = c.select, bold = true })
 
-      FzfLuaNormal = { link = 'NormalFloat' },
-      FzfLuaBorder = { link = 'FloatBorder' },
-      FzfLuaTitle = { link = 'FloatTitle' },
-      FzfLuaPreviewNormal = { link = 'NormalFloat' },
-      FzfLuaPreviewBorder = { link = 'FloatBorder' },
-      FzfLuaPreviewTitle = { link = 'FloatTitle' },
-      FzfLuaCursorLine = { bg = 'sel0' },
-      FzfLuaCursorLineNr = { fg = 'palette.blue', bg = 'sel0' },
-      FzfLuaFzfNormal = { fg = 'fg1', bg = 'NONE' },
-      FzfLuaFzfCursorLine = { fg = 'fg1', bg = 'sel0' },
-      FzfLuaFzfMatch = { fg = 'palette.blue' },
-      FzfLuaFilePart = { fg = 'fg1' },
-      FzfLuaDirPart = { fg = 'fg2' },
-      FzfLuaPathLineNr = { fg = 'palette.blue' },
-      FzfLuaPathColNr = { fg = 'palette.blue' },
+hl('StatusLine', { fg = c.ink, bg = c.paper_alt })
+hl('StatusLineNC', { fg = c.ink_faint, bg = c.paper_alt })
+hl('TabLine', { fg = c.ink_soft, bg = c.paper_alt })
+hl('TabLineFill', { bg = c.paper_alt })
+hl('TabLineSel', { fg = c.ink, bg = c.paper, bold = true })
+hl('Pmenu', { fg = c.ink, bg = c.paper_float })
+hl('PmenuBorder', { fg = c.blue, bg = c.paper_float })
+hl('PmenuSel', { fg = c.ink, bg = c.select })
+hl('PmenuSbar', { bg = c.paper_alt })
+hl('PmenuThumb', { bg = c.select })
 
-      BlinkCmpMenu = { link = 'Pmenu' },
-      BlinkCmpMenuBorder = { link = 'PmenuBorder' },
-      BlinkCmpMenuSelection = { link = 'PmenuSel' },
-      BlinkCmpDoc = { link = 'NormalFloat' },
-      BlinkCmpDocBorder = { link = 'FloatBorder' },
-      BlinkCmpDocSeparator = { link = 'FloatBorder' },
-      BlinkCmpSignatureHelp = { link = 'NormalFloat' },
-      BlinkCmpSignatureHelpBorder = { link = 'FloatBorder' },
-      BlinkCmpScrollBarThumb = { link = 'PmenuThumb' },
-      BlinkCmpScrollBarGutter = { link = 'PmenuSbar' },
-    },
-  },
-}
+hl('Comment', { fg = c.ink_faint, italic = true })
+hl('Constant', { fg = c.gold })
+hl('String', { fg = c.orange })
+hl('Character', { fg = c.orange })
+hl('Number', { fg = c.gold })
+hl('Boolean', { fg = c.gold })
+hl('Float', { fg = c.gold })
+hl('Identifier', { fg = c.ink })
+hl('Function', { fg = c.rose })
+hl('Statement', { fg = c.blue })
+hl('Conditional', { fg = c.blue })
+hl('Repeat', { fg = c.blue })
+hl('Label', { fg = c.blue })
+hl('Operator', { fg = c.ink })
+hl('Keyword', { fg = c.blue })
+hl('Exception', { fg = c.blue })
+hl('PreProc', { fg = c.blue_soft })
+hl('Include', { fg = c.blue })
+hl('Define', { fg = c.blue_soft })
+hl('Macro', { fg = c.blue_soft })
+hl('PreCondit', { fg = c.blue_soft })
+hl('Type', { fg = c.blue })
+hl('StorageClass', { fg = c.blue })
+hl('Structure', { fg = c.blue })
+hl('Typedef', { fg = c.blue })
+hl('Special', { fg = c.rose })
+hl('SpecialChar', { fg = c.rose })
+hl('Delimiter', { fg = c.ink_soft })
+hl('SpecialComment', { fg = c.ink_faint, italic = true })
 
--- setup must be called before loading
-vim.cmd 'colorscheme dawnfox'
+hl('@variable', { fg = c.ink })
+hl('@variable.builtin', { fg = c.blue_soft })
+hl('@parameter', { fg = c.ink })
+hl('@field', { fg = c.rose })
+hl('@property', { fg = c.rose })
+hl('@function', { fg = c.rose })
+hl('@function.call', { fg = c.rose })
+hl('@function.builtin', { fg = c.blue_soft })
+hl('@method', { fg = c.rose })
+hl('@method.call', { fg = c.rose })
+hl('@constructor', { fg = c.blue })
+hl('@keyword', { fg = c.blue })
+hl('@keyword.function', { fg = c.blue })
+hl('@keyword.operator', { fg = c.blue })
+hl('@keyword.return', { fg = c.blue })
+hl('@operator', { fg = c.ink })
+hl('@type', { fg = c.blue })
+hl('@type.builtin', { fg = c.blue })
+hl('@module', { fg = c.blue })
+hl('@constant', { fg = c.gold })
+hl('@string', { fg = c.orange })
+hl('@string.escape', { fg = c.rose })
+hl('@number', { fg = c.gold })
+hl('@boolean', { fg = c.gold })
+hl('@comment', { fg = c.ink_faint, italic = true })
+
+hl('DiagnosticError', { fg = c.red })
+hl('DiagnosticWarn', { fg = c.gold })
+hl('DiagnosticInfo', { fg = c.blue })
+hl('DiagnosticHint', { fg = c.green })
+
+hl('FzfLuaNormal', { link = 'NormalFloat' })
+hl('FzfLuaBorder', { link = 'FloatBorder' })
+hl('FzfLuaTitle', { link = 'FloatTitle' })
+hl('FzfLuaPreviewNormal', { link = 'NormalFloat' })
+hl('FzfLuaPreviewBorder', { link = 'FloatBorder' })
+hl('FzfLuaPreviewTitle', { link = 'FloatTitle' })
+hl('FzfLuaCursorLine', { bg = c.select })
+hl('FzfLuaCursorLineNr', { fg = c.blue, bg = c.select })
+hl('FzfLuaFzfNormal', { fg = c.ink, bg = c.paper_float })
+hl('FzfLuaFzfCursorLine', { fg = c.ink, bg = c.select })
+hl('FzfLuaFzfMatch', { fg = c.blue, bold = true })
+hl('FzfLuaFilePart', { fg = c.ink })
+hl('FzfLuaDirPart', { fg = c.ink_soft })
+hl('FzfLuaPathLineNr', { fg = c.blue })
+hl('FzfLuaPathColNr', { fg = c.blue })
+
+hl('BlinkCmpMenu', { link = 'Pmenu' })
+hl('BlinkCmpMenuBorder', { link = 'PmenuBorder' })
+hl('BlinkCmpMenuSelection', { link = 'PmenuSel' })
+hl('BlinkCmpDoc', { link = 'NormalFloat' })
+hl('BlinkCmpDocBorder', { link = 'FloatBorder' })
+hl('BlinkCmpDocSeparator', { link = 'FloatBorder' })
+hl('BlinkCmpSignatureHelp', { link = 'NormalFloat' })
+hl('BlinkCmpSignatureHelpBorder', { link = 'FloatBorder' })
+hl('BlinkCmpScrollBarThumb', { link = 'PmenuThumb' })
+hl('BlinkCmpScrollBarGutter', { link = 'PmenuSbar' })
