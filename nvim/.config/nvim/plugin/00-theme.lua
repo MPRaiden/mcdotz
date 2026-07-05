267514a2d6bab331fabbbf859257dcbdb5d54882
Marin Peric Radan <marin.peric@seekandhit.com>
2026-07-05 13:26:14 +0200

    custom theme p6

--- nvim/.config/nvim/plugin/00-theme.lua
+++ nvim/.config/nvim/plugin/00-theme.lua
@@ -1,149 +1,207 @@
-vim.o.termguicolors = true
+vim.pack.add {
+  {
+    src = 'https://github.com/rose-pine/neovim',
+    name = 'rose-pine',
+  },
+}
+require('rose-pine').setup {
+  variant = 'dawn', -- auto, main, moon, or dawn
+  dark_variant = 'main', -- main, moon, or dawn
+  dim_inactive_windows = false,
+  extend_background_behind_borders = true,
 
-local c = {
-  paper = '#f8f1e8',
-  paper_alt = '#f3e8dc',
-  paper_float = '#faf3eb',
-  ink = '#4e4945',
-  ink_soft = '#6b655e',
-  ink_faint = '#9a9086',
-  blue = '#467fbe',
-  blue_soft = '#6c98c0',
-  rose = '#cb8d97',
-  gold = '#c6a45d',
-  orange = '#e39a2d',
-  green = '#7ea17a',
-  red = '#c07a70',
-  select = '#e4d5c4',
-  search = '#ead9a7',
+  enable = {
+    terminal = true,
+    legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+    migrations = true, -- Handle deprecated options automatically
+  },
+
+  styles = {
+    bold = true,
+    italic = false,
+    -- transparency = true,
+  },
+
+  groups = {
+    border = 'muted',
+    link = 'iris',
+    panel = 'surface',
+
+    error = 'love',
+    hint = 'iris',
+    info = 'foam',
+    note = 'pine',
+    todo = 'rose',
+    warn = 'gold',
+
+    git_add = 'foam',
+    git_change = 'rose',
+    git_delete = 'love',
+    git_dirty = 'rose',
+    git_ignore = 'muted',
+    git_merge = 'iris',
+    git_rename = 'pine',
+    git_stage = 'iris',
+    git_text = 'rose',
+    git_untracked = 'subtle',
+
+    h1 = 'iris',
+    h2 = 'foam',
+    h3 = 'rose',
+    h4 = 'gold',
+    h5 = 'pine',
+    h6 = 'foam',
+  },
+
+  palette = {
+    dawn = {
+      base = '#f8f1e8',
+      surface = '#faf3eb',
+      overlay = '#f3e8dc',
+      muted = '#9a9086',
+      subtle = '#6b655e',
+      text = '#4e4945',
+      love = '#c07a70',
+      gold = '#c6a45d',
+      rose = '#cb8d97',
+      pine = '#467fbe',
+      foam = '#6c98c0',
+      iris = '#8b7bb2',
+      highlight_low = '#f3e8dc',
+      highlight_med = '#e4d5c4',
+      highlight_high = '#ead9a7',
+    },
+  },
+
+  -- NOTE: Highlight groups are extended (merged) by default. Disable this
+  -- per group via `inherit = false`
+  highlight_groups = {
+    NormalFloat = { bg = 'surface' },
+    FloatBorder = { fg = 'pine', bg = 'surface' },
+    FloatTitle = { fg = 'pine', bg = 'surface', bold = true },
+    CursorLine = { bg = 'overlay' },
+    CursorColumn = { bg = 'overlay' },
+    CursorLineNr = { fg = 'pine', bg = 'overlay', bold = true },
+    LineNr = { fg = 'muted', bg = 'base' },
+    SignColumn = { bg = 'base' },
+    FoldColumn = { fg = 'muted', bg = 'base' },
+    Folded = { fg = 'subtle', bg = 'overlay' },
+    Visual = { bg = 'highlight_med' },
+    Search = { fg = 'text', bg = 'highlight_high' },
+    IncSearch = { fg = 'base', bg = 'pine' },
+    MatchParen = { fg = 'rose', bg = 'highlight_med', bold = true },
+
+    StatusLine = { fg = 'text', bg = 'overlay' },
+    StatusLineNC = { fg = 'muted', bg = 'overlay' },
+    TabLine = { fg = 'subtle', bg = 'overlay' },
+    TabLineFill = { bg = 'overlay' },
+    TabLineSel = { fg = 'text', bg = 'base', bold = true },
+
+    Pmenu = { fg = 'text', bg = 'surface' },
+    PmenuBorder = { fg = 'pine', bg = 'surface' },
+    PmenuSel = { fg = 'text', bg = 'highlight_med' },
+    PmenuSbar = { bg = 'overlay' },
+    PmenuThumb = { bg = 'highlight_med' },
+
+    Comment = { fg = 'muted', italic = true },
+    Constant = { fg = 'gold' },
+    String = { fg = '#e39a2d' },
+    Character = { fg = '#e39a2d' },
+    Number = { fg = 'gold' },
+    Boolean = { fg = 'gold' },
+    Float = { fg = 'gold' },
+    Identifier = { fg = 'text' },
+    Function = { fg = 'rose' },
+    Statement = { fg = 'pine' },
+    Conditional = { fg = 'pine' },
+    Repeat = { fg = 'pine' },
+    Label = { fg = 'pine' },
+    Operator = { fg = 'text' },
+    Keyword = { fg = 'pine' },
+    Exception = { fg = 'pine' },
+    PreProc = { fg = 'foam' },
+    Include = { fg = 'pine' },
+    Define = { fg = 'foam' },
+    Macro = { fg = 'foam' },
+    PreCondit = { fg = 'foam' },
+    Type = { fg = 'pine' },
+    StorageClass = { fg = 'pine' },
+    Structure = { fg = 'pine' },
+    Typedef = { fg = 'pine' },
+    Special = { fg = 'rose' },
+    SpecialChar = { fg = 'rose' },
+    Delimiter = { fg = 'subtle' },
+
+    ['@variable'] = { fg = 'text' },
+    ['@variable.builtin'] = { fg = 'foam' },
+    ['@parameter'] = { fg = 'text' },
+    ['@field'] = { fg = 'rose' },
+    ['@property'] = { fg = 'rose' },
+    ['@function'] = { fg = 'rose' },
+    ['@function.call'] = { fg = 'rose' },
+    ['@function.builtin'] = { fg = 'foam' },
+    ['@method'] = { fg = 'rose' },
+    ['@method.call'] = { fg = 'rose' },
+    ['@constructor'] = { fg = 'pine' },
+    ['@keyword'] = { fg = 'pine' },
+    ['@keyword.function'] = { fg = 'pine' },
+    ['@keyword.operator'] = { fg = 'pine' },
+    ['@keyword.return'] = { fg = 'pine' },
+    ['@operator'] = { fg = 'text' },
+    ['@type'] = { fg = 'pine' },
+    ['@type.builtin'] = { fg = 'pine' },
+    ['@module'] = { fg = 'pine' },
+    ['@constant'] = { fg = 'gold' },
+    ['@string'] = { fg = '#e39a2d' },
+    ['@string.escape'] = { fg = 'rose' },
+    ['@number'] = { fg = 'gold' },
+    ['@boolean'] = { fg = 'gold' },
+    ['@comment'] = { fg = 'muted', italic = true },
+
+    DiagnosticError = { fg = 'love' },
+    DiagnosticWarn = { fg = 'gold' },
+    DiagnosticInfo = { fg = 'pine' },
+    DiagnosticHint = { fg = 'foam' },
+
+    FzfLuaNormal = { link = 'NormalFloat' },
+    FzfLuaBorder = { link = 'FloatBorder' },
+    FzfLuaTitle = { link = 'FloatTitle' },
+    FzfLuaPreviewNormal = { link = 'NormalFloat' },
+    FzfLuaPreviewBorder = { link = 'FloatBorder' },
+    FzfLuaPreviewTitle = { link = 'FloatTitle' },
+    FzfLuaCursorLine = { bg = 'highlight_med' },
+    FzfLuaCursorLineNr = { fg = 'pine', bg = 'highlight_med' },
+    FzfLuaFzfNormal = { fg = 'text', bg = 'surface' },
+    FzfLuaFzfCursorLine = { fg = 'text', bg = 'highlight_med' },
+    FzfLuaFzfMatch = { fg = 'pine', bold = true },
+    FzfLuaFilePart = { fg = 'text' },
+    FzfLuaDirPart = { fg = 'subtle' },
+    FzfLuaPathLineNr = { fg = 'pine' },
+    FzfLuaPathColNr = { fg = 'pine' },
+
+    BlinkCmpMenu = { link = 'Pmenu' },
+    BlinkCmpMenuBorder = { link = 'PmenuBorder' },
+    BlinkCmpMenuSelection = { link = 'PmenuSel' },
+    BlinkCmpDoc = { link = 'NormalFloat' },
+    BlinkCmpDocBorder = { link = 'FloatBorder' },
+    BlinkCmpDocSeparator = { link = 'FloatBorder' },
+    BlinkCmpSignatureHelp = { link = 'NormalFloat' },
+    BlinkCmpSignatureHelpBorder = { link = 'FloatBorder' },
+    BlinkCmpScrollBarThumb = { link = 'PmenuThumb' },
+    BlinkCmpScrollBarGutter = { link = 'PmenuSbar' },
+  },
+
+  before_highlight = function(group, highlight, palette)
+    -- Disable all undercurls
+    -- if highlight.undercurl then
+    --     highlight.undercurl = false
+    -- end
+    --
+    -- Change palette colour
+    -- if highlight.fg == palette.pine then
+    --     highlight.fg = palette.foam
+    -- end
+  end,
 }
 
-vim.cmd('highlight clear')
-if vim.fn.exists('syntax_on') == 1 then
-  vim.cmd('syntax reset')
-end
-vim.g.colors_name = 'paper_kakashi'
-
-local set = vim.api.nvim_set_hl
-local function hl(group, opts)
-  set(0, group, opts)
-end
-
-hl('Normal', { fg = c.ink, bg = c.paper })
-hl('NormalNC', { fg = c.ink, bg = c.paper })
-hl('NormalFloat', { fg = c.ink, bg = c.paper_float })
-hl('FloatBorder', { fg = c.blue, bg = c.paper_float })
-hl('FloatTitle', { fg = c.blue, bg = c.paper_float, bold = true })
-hl('ColorColumn', { bg = c.paper_alt })
-hl('CursorLine', { bg = c.paper_alt })
-hl('CursorColumn', { bg = c.paper_alt })
-hl('CursorLineNr', { fg = c.blue, bg = c.paper_alt, bold = true })
-hl('LineNr', { fg = c.ink_faint, bg = c.paper })
-hl('SignColumn', { bg = c.paper })
-hl('FoldColumn', { fg = c.ink_faint, bg = c.paper })
-hl('Folded', { fg = c.ink_soft, bg = c.paper_alt })
-hl('VertSplit', { fg = c.paper_alt, bg = c.paper })
-hl('WinSeparator', { fg = c.paper_alt, bg = c.paper })
-hl('Visual', { bg = c.select })
-hl('Search', { fg = c.ink, bg = c.search })
-hl('IncSearch', { fg = c.paper, bg = c.blue })
-hl('MatchParen', { fg = c.rose, bg = c.select, bold = true })
-
-hl('StatusLine', { fg = c.ink, bg = c.paper_alt })
-hl('StatusLineNC', { fg = c.ink_faint, bg = c.paper_alt })
-hl('TabLine', { fg = c.ink_soft, bg = c.paper_alt })
-hl('TabLineFill', { bg = c.paper_alt })
-hl('TabLineSel', { fg = c.ink, bg = c.paper, bold = true })
-hl('Pmenu', { fg = c.ink, bg = c.paper_float })
-hl('PmenuBorder', { fg = c.blue, bg = c.paper_float })
-hl('PmenuSel', { fg = c.ink, bg = c.select })
-hl('PmenuSbar', { bg = c.paper_alt })
-hl('PmenuThumb', { bg = c.select })
-
-hl('Comment', { fg = c.ink_faint, italic = true })
-hl('Constant', { fg = c.gold })
-hl('String', { fg = c.orange })
-hl('Character', { fg = c.orange })
-hl('Number', { fg = c.gold })
-hl('Boolean', { fg = c.gold })
-hl('Float', { fg = c.gold })
-hl('Identifier', { fg = c.ink })
-hl('Function', { fg = c.rose })
-hl('Statement', { fg = c.blue })
-hl('Conditional', { fg = c.blue })
-hl('Repeat', { fg = c.blue })
-hl('Label', { fg = c.blue })
-hl('Operator', { fg = c.ink })
-hl('Keyword', { fg = c.blue })
-hl('Exception', { fg = c.blue })
-hl('PreProc', { fg = c.blue_soft })
-hl('Include', { fg = c.blue })
-hl('Define', { fg = c.blue_soft })
-hl('Macro', { fg = c.blue_soft })
-hl('PreCondit', { fg = c.blue_soft })
-hl('Type', { fg = c.blue })
-hl('StorageClass', { fg = c.blue })
-hl('Structure', { fg = c.blue })
-hl('Typedef', { fg = c.blue })
-hl('Special', { fg = c.rose })
-hl('SpecialChar', { fg = c.rose })
-hl('Delimiter', { fg = c.ink_soft })
-hl('SpecialComment', { fg = c.ink_faint, italic = true })
-
-hl('@variable', { fg = c.ink })
-hl('@variable.builtin', { fg = c.blue_soft })
-hl('@parameter', { fg = c.ink })
-hl('@field', { fg = c.rose })
-hl('@property', { fg = c.rose })
-hl('@function', { fg = c.rose })
-hl('@function.call', { fg = c.rose })
-hl('@function.builtin', { fg = c.blue_soft })
-hl('@method', { fg = c.rose })
-hl('@method.call', { fg = c.rose })
-hl('@constructor', { fg = c.blue })
-hl('@keyword', { fg = c.blue })
-hl('@keyword.function', { fg = c.blue })
-hl('@keyword.operator', { fg = c.blue })
-hl('@keyword.return', { fg = c.blue })
-hl('@operator', { fg = c.ink })
-hl('@type', { fg = c.blue })
-hl('@type.builtin', { fg = c.blue })
-hl('@module', { fg = c.blue })
-hl('@constant', { fg = c.gold })
-hl('@string', { fg = c.orange })
-hl('@string.escape', { fg = c.rose })
-hl('@number', { fg = c.gold })
-hl('@boolean', { fg = c.gold })
-hl('@comment', { fg = c.ink_faint, italic = true })
-
-hl('DiagnosticError', { fg = c.red })
-hl('DiagnosticWarn', { fg = c.gold })
-hl('DiagnosticInfo', { fg = c.blue })
-hl('DiagnosticHint', { fg = c.green })
-
-hl('FzfLuaNormal', { link = 'NormalFloat' })
-hl('FzfLuaBorder', { link = 'FloatBorder' })
-hl('FzfLuaTitle', { link = 'FloatTitle' })
-hl('FzfLuaPreviewNormal', { link = 'NormalFloat' })
-hl('FzfLuaPreviewBorder', { link = 'FloatBorder' })
-hl('FzfLuaPreviewTitle', { link = 'FloatTitle' })
-hl('FzfLuaCursorLine', { bg = c.select })
-hl('FzfLuaCursorLineNr', { fg = c.blue, bg = c.select })
-hl('FzfLuaFzfNormal', { fg = c.ink, bg = c.paper_float })
-hl('FzfLuaFzfCursorLine', { fg = c.ink, bg = c.select })
-hl('FzfLuaFzfMatch', { fg = c.blue, bold = true })
-hl('FzfLuaFilePart', { fg = c.ink })
-hl('FzfLuaDirPart', { fg = c.ink_soft })
-hl('FzfLuaPathLineNr', { fg = c.blue })
-hl('FzfLuaPathColNr', { fg = c.blue })
-
-hl('BlinkCmpMenu', { link = 'Pmenu' })
-hl('BlinkCmpMenuBorder', { link = 'PmenuBorder' })
-hl('BlinkCmpMenuSelection', { link = 'PmenuSel' })
-hl('BlinkCmpDoc', { link = 'NormalFloat' })
-hl('BlinkCmpDocBorder', { link = 'FloatBorder' })
-hl('BlinkCmpDocSeparator', { link = 'FloatBorder' })
-hl('BlinkCmpSignatureHelp', { link = 'NormalFloat' })
-hl('BlinkCmpSignatureHelpBorder', { link = 'FloatBorder' })
-hl('BlinkCmpScrollBarThumb', { link = 'PmenuThumb' })
-hl('BlinkCmpScrollBarGutter', { link = 'PmenuSbar' })
+vim.cmd 'colorscheme rose-pine'
