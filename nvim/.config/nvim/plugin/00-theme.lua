1781f7a715dfcfca9617f3b785fcb5fb0c1262b3
Marin Peric Radan <marin.peric@seekandhit.com>
2026-07-25 15:07:53 +0200

    sync

--- nvim/.config/nvim/plugin/00-theme.lua
+++ nvim/.config/nvim/plugin/00-theme.lua
@@ -3,9 +3,9 @@ vim.pack.add { { src = 'https://github.com/catppuccin/nvim', name = 'catppuccin'
 require('catppuccin').setup {
   flavour = 'auto', -- latte, frappe, macchiato, mocha
   background = {
-    light: 'latte',
-    dark: 'macchiato',
-    },
+    light = 'latte',
+    dark = 'macchiato',
+  },
   transparent_background = true, -- disables setting the background color.
   float = {
     transparent = true, -- enable transparent floating windows
